3f44e43c0a44b48911ea7dcfa5de8163aeb12476
Gleb Zikunov <zikunovga@gmail.com>
2024-05-22 14:23:43 +0300

    fix: search non premium community if user isn't logged in

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,3 +1,5 @@
+import { hasSubscription } from "@/helpers/billing"
+import { getAuthSession } from "@/lib/auth"
 import { db } from "@/lib/db"
 
 export async function GET(req: Request) {
@@ -6,11 +8,31 @@ export async function GET(req: Request) {
 
   if (!q) return new Response("Invalid query", { status: 400 })
 
+  const session = await getAuthSession()
+  const hasSub = await hasSubscription()
+
+  if (session && hasSub) {
+    const results = await db.community.findMany({
+      where: {
+        name: {
+          startsWith: q,
+        },
+      },
+      include: {
+        _count: true,
+      },
+      take: 5,
+    })
+
+    return new Response(JSON.stringify(results))
+  }
+
   const results = await db.community.findMany({
     where: {
       name: {
         startsWith: q,
       },
+      isPremium: false,
     },
     include: {
       _count: true,
--- src/middleware.ts
+++ src/middleware.ts
@@ -14,5 +14,6 @@ export const config = {
     "/c/create",
     "/c",
     "/premium",
+    "/bookmarks",
   ],
 }
